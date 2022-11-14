ef38e450197a67eb6e6d225454fe754078529a0d
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-14 08:39:40 +0000

    Create helloworld.cpp

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+  
+    std::cout << "hello world.";
+    return 0;
+  
+  }
+
+
+#include<iostream>
+#include<cstdlib>
+
+main() {
+   int max;
+   max = 6;
+   srand(time(0));
+  
+   int tips[6] = {"Is that really what you should be doing?", "Questionable selection of operating systems", "Don't mind me", "Chrome is an abomination of a \'browser\'", "my favourite distro is artix", "UwU compile your kernel fwom source to squeeze extra performance"};
+   cout << tips[rand()%max];
+}
