3d5fae1e36b6a826201f87ec82820b5d062ef6b4
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-14 08:27:10 +0000

    Create helloworld.java

---
+++ helloworld/helloworld.java
@@ -0,0 +1,5 @@
+public class java {
+    public static void main(String[] args) {
+        System.out.println("*clap* public *clap* static *clap* voic *clap* main *clap* string *clap* args! what part of that do you not understand????!!!!!11!!11!!11!1");
+    }
+}
