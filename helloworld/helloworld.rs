3735358b6324c13b35e8231ff1bce9a17ca449c5
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-14 08:24:00 +0000

    Create helloworld.rs

---
+++ helloworld/helloworld.rs
@@ -0,0 +1,17 @@
+fn main(){
+
+    println!("Hello World.");
+	println!("Base 10:               {}",   42069); 
+    println!("Base 2:       {:b}", 42069); 
+    println!("Base 8:        {:o}", 42069);
+    println!("Base 16: {:x}", 42069);
+
+
+	println!("{c} {h} {v} {d}",
+         	 c="I'd like to",
+             h="interject",
+             v="for a"
+			 d="moment.");
+	println!("see ya");
+
+}
