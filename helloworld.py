efbfb074593539efe5c9ea211f02e8876712d9ba
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-14 08:04:34 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,12 @@
 #pls install ctypes and rust as dependencies & then cargo build the rust file & save it in somehwere like /usr/share or /usr/bin
 
-import ctypes
+# import ctypes
 
-rust = ctypes.CDLL("target/release/librust_lib.so")
-
-if __name__ == "__main__":
-  K = ("hellow world:)".encode("utf-8")
-       rust.print_string(K)
+#rust = ctypes.CDLL("target/release/librust_lib.so")
+#
+#if __name__ == "__main__":
+#  K = ("hellow world:)".encode("utf-8")
+#       rust.print_string(K)
 
     
        
