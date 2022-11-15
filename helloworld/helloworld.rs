abaa9a4c26317c01bacc39189428af18e530c6f5
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-15 10:23:17 +0000

    Update helloworld.rs

--- helloworld/helloworld.rs
+++ helloworld/helloworld.rs
@@ -1,17 +1,18 @@
 fn main(){
 
-    println!("Hello World.");
+	println!("Hello World.");
 	println!("Base 10:               {}",   42069); 
-    println!("Base 2:       {:b}", 42069); 
-    println!("Base 8:        {:o}", 42069);
+	println!("Base 2:       {:b}", 42069); 
+	println!("Base 8:        {:o}", 42069);
     println!("Base 16: {:x}", 42069);
 
 
 	println!("{c} {h} {v} {d}",
-         	 c="I'd like to",
-             h="interject",
-             v="for a"
-			 d="moment.");
+         	 c="A",
+             h="C",
+             v="A"
+			 d="B!!!!!!!!!");
+	
 	println!("see ya");
 
 }
