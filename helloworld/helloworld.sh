9a182e23018305bfcd873c8b486e78dabed95462
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-15 09:24:51 +0000

    Create helloworld.sh

---
+++ helloworld/helloworld.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+echo "Hello world. Today we are going to play a game of Russian Roulette. But first, I will tell you that I spare all linux and bsd users so if u use macOS then skill issue on ur part"
