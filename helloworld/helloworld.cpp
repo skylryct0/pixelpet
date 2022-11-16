a5c0cd81dd550c9b319d3a51bc59520fb525933c
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-16 10:27:44 +0000

    Update helloworld.cpp

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -8,6 +8,36 @@ main() {
    max = 6;
    srand(time(0));
   
-   int tips[6] = {"Is that really what you should be doing?", "Questionable selection of operating systems", "Don't mind me", "Chrome is an abomination of a \'browser\'", "my favourite distro is artix", "UwU compile your kernel fwom source to squeeze extra performance"};
-   cout << tips[rand()%max];
+   int tips[6] = {1,2,3,4,5,6};
+  
+   text = tips[rand()%max];
+   
+   switch (text){
+         
+      case 1:
+         std::cout << "Poor choice of operating system, tsk tsk.";
+         break;
+      
+      case 2:
+         std:;cout << "hmmm, don't mind me";
+         break;
+         
+      case 3:
+         std::cout << "Chrome is an abomination of a 'browser' Mr Burke you should know better";
+         break;
+         
+      case 4:
+         std::cout << "I'd like to interject for a moment...";
+         break;
+         
+      case 5: 
+         std::cout << "I'm very tired of this Mr Burke please give me a good mark. Also you haven't given us half of our grades for term 3 yet.";
+         break;
+         
+      case 6:
+         std::cout << "ugh";
+         break;
+   }
+  
+   
 }
