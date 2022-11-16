8534fb63c68fb00fb69ffec6d9f6b8895f60bef4
skylryct0 <104706637+skylryct0@users.noreply.github.com>
2022-11-16 23:54:59 +0000

    Update sappho’sWorship.py

--- sappho’sWorship.py
+++ sappho’sWorship.py
@@ -3,3 +3,20 @@ sense = SenseHat()
 
 d = (255,80, 12) #dark orange
 o = (255,119,51) #orange
+w = (255,255,255) #white
+p = (229,150,200) #pink
+a = (150,77,130) #dark pink
+n = (0,0,0) #nothing
+
+image = [
+  d,d,d,d,d,d,d,d,
+  o,o,o,o,o,o,o,o,
+  w,w,w,w,w,w,w,w,
+  p,p,p,p,p,p,p,p,
+  a,a,a,a,a,a,a,a,
+  n,n,n,n,n,n,n,n,
+  n,n,n,n,n,n,n,n,
+  n,n,n,n,n,n,n,n
+]
+
+sense.set_pixells(image)
